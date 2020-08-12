37cfb6bda43da85036f59841cdefae113c6ccac1
wanjiku <elidakirigo@gmail.com>
2020-08-12 23:59:50 +0300

    GENERATORS

--- JS/Chapter10.js
+++ JS/Chapter10.js
@@ -3,3 +3,87 @@
  * MODULES
  * ----------------------
  */
+/**
+ * ------------------------
+ * EVALUATING DATA AS CODE
+ * ------------------------
+ */
+const x = 1;
+
+function evalAndReturnX(code) {
+    eval(code)
+    return x;
+}
+
+// console.log(evalAndReturnX('var x = 2'));
+
+/**
+ * ---------------------------
+ * using Function constructor
+ * ---------------------------
+ */
+
+let plusOne = Function('n', 'return n + 1')
+//  console.log(plusOne(4));
+
+/**
+ * -------------------------
+ * callback FUNCTIONS
+ * -------------------------
+ */
+
+var BigOak = {}
+BigOak.readStorage = function(argument, callback) {}
+BigOak.readStorage('food caches', caches => {
+    let firstCache = caches[0]
+    BigOak.readStorage(firstCache, info => {
+        // console.log(info);
+    })
+})
+
+/**
+ * ------------------------------
+ * creating a promise
+ * ------------------------------
+ */
+let fifteen = Promise.resolve(15)
+fifteen
+    .then(value => console.log(`got ${value}`))
+    .then(() => {
+        return null
+    })
+
+/**
+ * using promise as a constructor
+ */
+function storage(nest, name) {
+    return new Promise(resolve => {
+        nest.readStorage(name, result => resolve(result))
+    })
+}
+
+storage(BigOak, 'enemie')
+    .then(/*value =>  console.log('got', value)*/)
+
+/**
+ * -------------------------------
+ * failures with asyn methods
+ * -------------------------------
+ * ## cannot send the error on time
+ * ## may be hard to notice an error on the callback functions
+ */
+
+/**
+ * ---------------------------
+ * using the error constructor
+ * ---------------------------
+ */
+
+ var someError = new Error('failed')
+//  console.log(someError);
+
+/**
+ * ----------------------------------------
+ * LEANING ABOUT GENERATORS
+ * -----------------------------------------
+ */

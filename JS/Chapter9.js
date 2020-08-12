47ea986ed4973d1f62d0d2e216912ed8007fde00
wanjiku <elidakirigo@gmail.com>
2020-08-12 18:35:45 +0300

    REGULAR EXPRESSIONS

---
+++ JS/Chapter9.js
@@ -0,0 +1,31 @@
+/**
+ * -----------------------------------
+ * using REGULAR EXPRESSIONS
+ * -----------------------------------
+ */
+// let myreg = new RegExp('abc')
+// console.log(/abc/.test('abcdef'));
+// console.log(/[0123456789]/.test('in 1992'));
+// console.log(/[0-9]/.test('in 1992'));
+
+/**
+ * -------------------------
+ * learning the replace method
+ * , replace is also part of the regex
+ * ---------------------------
+ */
+
+//  console.log('papa'.replace('p','m'));
+//  console.log('pkaujpau'.replace(/[ap]/,'m'));
+//  console.log('pkaujpau'.replace(/[ap]/g,'m'));
+
+ /**
+  * --------------------------------
+  * using search in REGEX
+  * --------------------------------
+  */
+//  console.log('word'.search(/[d]/));
+
+ let regex = /[elida]/
+ let mynamein = 'yghbnjiuyytghbvcxelidadyuhijokm'
+//  console.log(mynamein.search(regex));

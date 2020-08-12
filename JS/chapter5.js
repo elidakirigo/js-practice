cbe5ede429c5efa8f4d4e8fd4c0c327b2dec6723
wanjiku <elidakirigo@gmail.com>
2020-08-12 06:14:08 +0300

    chapter6

---
+++ JS/chapter5.js
@@ -0,0 +1,26 @@
+// using Map
+let m = [2, 3, 1, 4, 5, 3]
+let n = m.map((minus,times) => {
+    return minus * minus + times;
+})
+
+let b = m.filter(fill => {
+    if (fill % 2 == 0) return fill;
+})
+// console.log(b);
+
+v = m.reduce(remove=>{
+   return remove;
+})
+// console.log(n);
+
+/**
+ * --------------------
+ * CLASSES
+ * --------------------
+ */
+let object = new class {
+    getword() {
+        return 'hello'
+    }
+}
---
+++ JS/chapter6.js
@@ -0,0 +1,7 @@
+var ages = new Map();
+ages.set('borris', 32)
+ages.set('agnes', 32)
+ages.set('jen', 32)
+console.log(ages);
+console.log(ages.get('jen'));
+console.log(ages.has('jen')); //return a boolean if the property is available

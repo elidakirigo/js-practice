c34581185785d85b4de8bcc78be6f95f1e7ed7b1
wanjiku <elidakirigo@gmail.com>
2020-08-13 20:25:36 +0300

    CANVAS

---
+++ JS/Chapter17.js
@@ -0,0 +1,13 @@
+/**
+ * ------------------------------------------------------
+ *                CANVAS
+ *              **********
+ *              **********
+ * -using a canvas is one of the ways to display praphics
+ * ------------------------------------------------------
+ */
+
+ /**
+  * using 2d - two dimention objects
+  * using webgl - three dimensional objects
+  */
